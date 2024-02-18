8e71b3f5c7f2e43744c6f2652bb3c235f649fa0b
Fall_zhang <zhangbofei@163.com>
2024-02-18 11:49:57 +0800

    更新多个 column 的组件

--- src/layout/GridColumn.vue
+++ src/layout/GridColumn.vue
@@ -6,11 +6,13 @@
 
 <script lang="ts" setup>
 const prop = defineProps({
+  // 单个子项的最大宽度
   max: {
     require: true,
     type: Number,
     default: 260
   },
+  // 单个最大宽度
   width: {
     require: true,
     type: Number,
@@ -26,7 +28,6 @@ function resizeContainer() {
     rowCount.value = 1
   } else {
     rowCount.value = columnCount
-
   }
 }
 onMounted(() => {
@@ -48,72 +49,6 @@ onUnmounted(() => {
   gap: 14px;
   padding: 20px;
   box-sizing: border-box;
-
-  .chartItem {
-    display: flex;
-    border-radius: 8px;
-    // height: 180px;
-    flex: 1;
-    box-sizing: border-box;
-    min-width: 280px;
-    // max-height: 260px;
-    border: 1px solid var(--gray-1);
-    box-shadow: var(--box-shadow-2);
-    flex-direction: column;
-    overflow: hidden;
-    cursor: pointer;
-
-    >.headText {
-      padding: 8px 8px 0;
-    }
-
-    >.itemImg {
-      display: flex;
-      padding: 12px 0;
-      max-width: 100%;
-      max-height: 100%;
-      flex: 1;
-    }
-
-    >.itemText {
-      border-top: 1px solid var(--gray-1);
-      display: flex;
-      padding: 8px 18px 12px;
-      justify-content: space-between;
-      align-items: center;
-    }
-  }
 }
 
-.option {
-  width: 40px;
-  display: flex;
-  align-items: center;
-  font-size: 16px;
-  margin-right: 6px;
-
-  .star {
-    display: flex;
-    padding: 4px;
-    align-items: center;
-    transition: 0.2s;
-
-    &:hover {
-      color: orange;
-    }
-  }
-
-  .more {
-    font-size: 18px;
-    display: flex;
-    padding: 4px;
-    align-items: center;
-    border-radius: 4px;
-
-    &:hover {
-      background-color: var(--gray-1);
-    }
-
-  }
-}
 </style>
