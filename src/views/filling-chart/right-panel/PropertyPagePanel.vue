30d34f9dbb3a35eae57c02049bc7291631bb58dc
fall-zhang <zhangbofei@163.com>
2024-07-08 09:09:29 +0800

    feat(component): 🚀 更新组件，依赖

--- src/views/filling-chart/right-panel/PropertyPagePanel.vue
+++ src/views/filling-chart/right-panel/PropertyPagePanel.vue
@@ -50,8 +50,8 @@ import FormItem from './components/FormItem.vue'
 import { Right as IconRight, Return as IconReturn, } from '@icon-park/vue-next'
 import formOptionList from './right-property'
 import HelpTooltip from './components/HelpTooltip.vue'
-
 import { ref } from 'vue'
+
 import { deepClone } from '@/utils/utils'
 const prop = defineProps({
   receiveValue: {
