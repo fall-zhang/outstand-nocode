ae0682ab08cc20523b41b95e1ab33ed409e6c0b5
fall-zhang <zhangbofei@163.com>
2024-09-20 16:44:25 +0800

    更新 icon 的 tooltip 组件

---
+++ src/components/more-layer/tooltip/IconTooltip.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-tooltip placement="top" :show-after="300">
+    <div class="cursor-pointer">
+      <slot></slot>
+    </div>
+    <template #content>
+      {{ tip }}
+    </template>
+  </el-tooltip>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  tip: {
+    require: false,
+    default: '',
+    type: String
+  }
+})
+
+</script>
+
+<style lang="scss" scoped>
+.cursor-pointer {
+  cursor: pointer;
+}
+</style>
